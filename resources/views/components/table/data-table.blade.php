de1acb643d5c3dc0a1f8ab5f7245a2b6b6f3f0ac
syahrul <syahrul4w@gmail.com>
2024-05-22 01:57:46 +0700

    fix: bug at scrollbar

--- resources/views/components/table/data-table.blade.php
+++ resources/views/components/table/data-table.blade.php
@@ -1,19 +1,20 @@
-<div class="flex flex-col justify-between w-full h-full overflow-hidden fade-in xl:overflow-y-auto lg:overflow-x-auto">
+<div
+    class="flex flex-col justify-between w-full h-full overflow-hidden 2xl:p-0 lg:px-2 fade-in xl:overflow-y-auto lg:overflow-x-auto">
     <table class="table-auto w-full xl:text-sm lg:text-xs font-medium" id="dataTable">
         <thead class="bg-[#F5F7F9]">
-            <tr class="text-Neutral/70">
+        <tr class="text-Neutral/70">
 
-                @foreach ($headers as $index => $header)
-                    @if ($index == 0)
-                        <th class="py-3 pl-6 pr-3 rounded-tl-2xl text-start">{{ $header }}</th>
-                    @elseif($index == count($headers) - 1)
-                        <th class="px-6 py-3 rounded-tr-2xl text-start">{{ $header }}</th>
-                    @else
-                        <th class="px-6 py-3 text-start">{{ $header }}</th>
-                    @endif
-                @endforeach
+            @foreach ($headers as $index => $header)
+                @if ($index == 0)
+                    <th class="py-3 pl-6 pr-3 rounded-tl-2xl text-start">{{ $header }}</th>
+                @elseif($index == count($headers) - 1)
+                    <th class="px-6 py-3 rounded-tr-2xl text-start">{{ $header }}</th>
+                @else
+                    <th class="px-6 py-3 text-start">{{ $header }}</th>
+                @endif
+            @endforeach
 
-            </tr>
+        </tr>
         </thead>
         @if ($dt->isEmpty())
             <tr>
@@ -23,12 +24,12 @@
             </tr>
         @else
             <tbody class="transition ease-in-out duration-200" id="bodyTable">
-                {{ $slot }}
+            {{ $slot }}
             </tbody>
         @endif
     </table>
     @if (Route::currentRouteName() !== 'bansos.show')
-        <div class="flex justify-between 2xl:my-0 xl:my-4 items-center" id="pagin">
+        <div class="flex justify-between 2xl:my-4 xl:my-4 items-center" id="pagin">
             <div class="flex items-center list-none gap-2">
                 {{ $dt->links() }}
             </div>
