b5f3c9d404f15ecd53656373f5bf1be8bb852c3d
syahrul <syahrul4w@gmail.com>
2024-05-25 17:12:54 +0700

    update(route): simplify route for bansos

--- routes/Handler/web/bansos.php
+++ routes/Handler/web/bansos.php
@@ -2,15 +2,15 @@
 Route::middleware(['auth', 'role:RW'])->group(function () {
     Route::resource('/bansos', \App\Http\Controllers\BansosController::class)->except(['create', 'destroy']);
 
-    Route::group(['prefix' => 'bansos'], function () {
+    Route::group(['prefix' => 'bansos', 'controller' => \App\Http\Controllers\BansosController::class], function () {
         Route::group(['prefix' => 'calculate'], function () {
-            Route::get('/edas', [\App\Http\Controllers\BansosController::class, 'edas']);
-            Route::get('/mabac', [\App\Http\Controllers\BansosController::class, 'mabac']);
+            Route::get('/edas', 'edas');
+            Route::get('/mabac', 'mabac');
         });
 
         Route::group(['prefix' => 'check'], function () {
-            Route::get('/edas', [\App\Http\Controllers\BansosController::class, 'checkEdas']);
-            Route::get('/mabac', [\App\Http\Controllers\BansosController::class, 'checkMabac']);
+            Route::get('/edas', 'checkEdas');
+            Route::get('/mabac', 'checkMabac');
         });
     });
 });
