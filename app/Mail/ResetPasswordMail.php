48df3c178c47bb30662af9451b015e95a59679f4
syahrul <syahrul4w@gmail.com>
2024-06-13 02:29:45 +0700

    update mail

--- app/Mail/ResetPasswordMail.php
+++ app/Mail/ResetPasswordMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -30,6 +31,7 @@ class ResetPasswordMail extends Mailable
     {
         return new Envelope(
             subject: 'Reset Password Mail',
+            from: new Address('siwarga13@gmail.com', 'Siwarga13')
         );
     }
 
