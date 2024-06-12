35894bddefae3d7663b048ee5a3f5a0d45fa3304
syahrul <syahrul4w@gmail.com>
2024-06-12 16:56:08 +0700

    fix: bug at mail

--- routes/Handler/web/auth.php
+++ routes/Handler/web/auth.php
@@ -2,7 +2,7 @@
 Route::get('/login', [\App\Http\Controllers\auth\AuthController::class, 'index'])->name('login');
 Route::post('/login', [\App\Http\Controllers\auth\AuthController::class, 'auth'])->name('login.auth');
 Route::post('/logout', [\App\Http\Controllers\auth\AuthController::class, 'logout'])->name('logout');
-Route::get('/forgot-password/{token}', [\App\Http\Controllers\auth\AuthController::class, 'forgotPassword'])->name('forgot-password');
+Route::get('/forgot-password/{token}', [\App\Http\Controllers\auth\AuthController::class, 'forgotPassword']);
 Route::get('/send-email', [\App\Http\Controllers\auth\AuthController::class, 'sendEmailForm'])->name('send-email');
 Route::post('/send-email', [\App\Http\Controllers\auth\AuthController::class, 'sendEmail'])->name('sendEmail');
 Route::post('/forgot-password', [\App\Http\Controllers\auth\AuthController::class, 'sendForgotPassword'])->name('forgot-password.send');
